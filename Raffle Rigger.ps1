1c739a87db1af7c637b70cd82ef1255ef4ce7cdb
Hrabosky <hraboskyjr@gmail.com>
2018-05-25 11:54:49 -0500

    Update Raffle Rigger.ps1

--- Raffle Rigger.ps1
+++ Raffle Rigger.ps1
@@ -1,143 +1,167 @@
-﻿Add-Type -AssemblyName Microsoft.VisualBasic
+Add-Type -AssemblyName Microsoft.VisualBasic
 <#
-    _   __              ____    __              _     
-   / | / /__  _________/ / /_  / /___  ____ _  (_)___ 
+    _   __              ____    __              _
+   / | / /__  _________/ / /_  / /___  ____ _  (_)___
   /  |/ / _ \/ ___/ __  / __ \/ / __ \/ __ `/ / / __ \
  / /|  /  __/ /  / /_/ / /_/ / / /_/ / /_/ / / / /_/ /
-/_/ |_/\___/_/   \__,_/_.___/_/\____/\__, (_)_/\____/ 
-                                    /____/            
+/_/ |_/\___/_/   \__,_/_.___/_/\____/\__, (_)_/\____/
+                                    /____/
 
- ____ ____ ____ ____ ____ ____ 
+ ____ ____ ____
+||H |||W |||S ||
+||__|||__|||__||
+|/__\|/__\|/__\|
+ ____ ____ ____ ____ ____ ____
 ||R |||a |||f |||f |||l |||e ||
 ||__|||__|||__|||__|||__|||__||
 |/__\|/__\|/__\|/__\|/__\|/__\|
- ____ ____ ____ ____ ____ ____ 
+ ____ ____ ____ ____ ____ ____
 ||R |||i |||g |||g |||e |||r ||
 ||__|||__|||__|||__|||__|||__||
 |/__\|/__\|/__\|/__\|/__\|/__\|
 
-V1.0
+V1.2
 
-Hey all, thank you for your interest in my script. 
-You may not know me, but my name is John and I have a site called nerdblog.io
-I will 
+Hey All, thank you for your interest in my script.
+My name is John and I have a site called nerdblog.io
 
 Improvements to come:
--Ability to blacklist users
 -Multiple sculpts with different quantities & prices
--Weighted raffles, based on users DKP points (Binge's Idea).
 -Nerdblog.io auto win
 
+The idea is you have a google form for your raffle, with the fields PaypalEmail, FirstName, LastName, Address,
+and a yes or no question if they're international. You would then export your results from your google sheets
+into an excel spreadsheet with the following cell values, A1 = "PaypalEmail", B1 = "FirstName", C1 = "LastName", D1 = "International",
+E1 = "Address". Save the excel file as a .csv (you can also do it with notepad, but have to separate with commas.)
+The Data object has a few fields for your paypal invoicing that you can write in. Should be at line # 149
 #>
 
-$AmountOfWinners = Read-Host "How many individual winners?" #This is the amount of people to win
-$RaffleEntriesCsv = ".\RaffleEntries.csv" <#
-This is the file location of your keycaps.csv file;
-The idea is you have a google form for your raffle, with the fields paypal email, first name, last name,
-and a yes or no question if they're international. You would then export your results from your google sheets
-into an excel spreadsheet with the following cell values, A1 = "paypal", B1 = "fname", C1 = "lname", D1 = "international".
-Save the excel file as a .csv (you can also do it with notepad, but have to separate with commas.)
-#>
-$WinnerPaypalCsv = ".\Winners.csv" #This is the location of where it will save the text file.
+#This is the amount of people to win
+$AmountOfWinners = Read-Host "How many individual winners?"
+
+#This is the file location of your raffle entries.
+$RaffleEntriesCsv = ".\RaffleEntries.csv"
+
+#This is the location of where it will save the text file.
+$WinnerPaypalCsv = ".\Winners.csv"
+#Banned users.
+$BlacklistCsv = ".\Blacklist.csv"
+#Imports blacklist
+$BlackListUsers = Import-Csv $BlacklistCsv | sort Address -Unique
+
 
 #Prompt user for necessary information (paypal invoicing).
 $KeycapName = Read-Host "What is the keycap name you are raffling?"
 $Keycapvalue = Read-Host "How much are you selling the $KeycapName for?"
 
-$EntryList = Import-Csv $RaffleEntriesCsv | sort paypal -Unique
-<#the line above imports the entries and removes duplicates based on Paypal emails addresses.
-I have forseen issues if a husband and wife use the same paypal email for example;
-or if someone wants to be sneaky and link a different email to their paypal account.
-My thought to ignore duplicates would be by delivery address; but didn't include it for this script.
-#>
+#The line below imports the entries and removes duplicates based on addresses.
+$EntryList = Import-Csv $RaffleEntriesCsv | sort Address -Unique
 
 #Declare arrays | Variables
-$PaypalArray = @()
 $PaypalEmail = @()
 $International = @()
-$ShippingArray = @()
-$WinnerArray = @()
+$Firstname = @()
+$LastName = @()
+$Address = @()
+$Entry = @()
 $ShippingCost = ""
-$data = ""
+$Data = ""
 
-#Cycles through each raffle entry, and stores their paypal email, along with if they're international or not.
-ForEach ($u in $EntryList){
-$PaypalEmail += $($u.paypal)
-$International += $($u.international)
+<#Cycles through each raffle entry, compares it against the blacklist and stores
+their paypal email, along with if they're international or not.
+It also generates a random number 1-1000 and stores the combined points in a new object. #>
+foreach ($u in $EntryList) {
+	if ($BlackListUsers | Where-Object { $_.Address -eq $($u.Address) }) {
+		Write-Host "" $u.Address " is banned"
+	} else {
+		$PaypalEmail = $($u.PaypalEmail)
+		$International = $($u.International)
+		$FirstName = $($u.FirstName)
+		$LastName = $($u.LastName)
+		$Address = $($u.Address)
+		$CombinedPoints = Get-Random 1000
+
+		$Entry += New-Object PSObject -Property @{
+			PaypalEmail = $PaypalEmail
+			International = $International
+			FirstName = $Firstname
+			LastName = $LastName
+			Address = $Address
+			Points = $Points
+			CombinedPoints = [int]$CombinedPoints
+		}
+	}
 }
 
-#Adds both entries to a jagged array
-$PaypalArray = $PaypalEmail,$International
+#Write winners to new object
+$Winners = $Entry | Sort-Object -Property CombinedPoints -Descending | Select-Object -First $AmountOfWinners
 
-
-#Picks the winners based on how the $AmountOfWinners variable | will not chose same winner twice.
-$WinnerArray += Get-Random -InputObject $PaypalArray[0] -Count $AmountOfWinners
-
-#Imports an array so you can append.
-if (Test-Path $WinnerPaypalCsv){
-    [array]$data = Import-Csv -Path $WinnerPaypalCsv
+#Imports the Winners csv file.
+if (Test-Path $WinnerPaypalCsv) {
+	[array]$Data = Import-Csv -Path $WinnerPaypalCsv
 }
 
-#Finds the index of the winners from the first array, and finds the if they are international or not.
-For ($i=0; $i -lt $WinnerArray.Length; $i++){
-$Search = $PaypalArray[0].IndexOf($WinnerArray[$i])
+#Export winners to Paypal csv
+foreach ($w in $Winners) {
 
-#converts a yes or no to a dollar value, change th values here if you want to adjust shipping pricing.
-if ($PaypalArray[1][$Search] -eq 'Yes'){
-    $ShippingCost = '13.00'
-}Else{
-   $ShippingCost = '5.00'
-}
+	if ($w.International -eq 'Yes') {
+		$ShippingCost = '13.00'
+	} else {
+		$ShippingCost = '5.00'
+	}
 
-<#
+	$Data += New-Object PSObject -Property @{
+		'Recipient Email' = $w.PaypalEmail
+		'Recipient First Name' = $w.FirstName
+		'Recipient Last Name' = $w.LastName
+		'Invoice Number' = ""
+		'Due Date' = ""
+		'Reference' = ""
+		'Item Name' = $KeycapName
+		'Description' = ""
+		'Item Amount' = $Keycapvalue
+		'Shipping Amount' = $ShippingCost
+		'Discount' = ""
+		'Currency' = "USD"
+		'Note to Customer' = "Thank you for supporting Nerdblog.io!"
+		'Terms and Conditions' = ""
+		'Memo to Self' = ""
+	}
+
+	<#
 Adds the winners to csv file. / the properties below are what gets exported, while only the necessary columns
 were added, you could hard code in due date, or Currency to USD... etc
 #>
-$data += New-Object PSObject -Property @{
-    'Recipient Email' = $WinnerArray[$i]
-    'Recipient First Name' = ""
-    'Recipient Last Name' = ""
-    'Invoice Number' = ""
-    'Due Date' = ""
-    'Reference' = ""
-    'Item Name' = $KeycapName
-    'Description' = ""
-    'Item Amount' = $Keycapvalue
-    'Shipping Amount' = $ShippingCost
-    'Discount' = ""
-    'Currency' = ""
-    'Note to Customer' = ""
-    'Terms and Conditions' = ""
-    'Memo to Self' = ""
+
+	$Data | Select-Object "Recipient Email","Recipient First Name","Recipient Last Name","Invoice Number","Due Date","Reference",
+	"Item Name","Description","Item Amount","Shipping Amount","Discount","Currency","Note to Customer","Terms and Conditions","Memo to Self" | Export-Csv -Path $WinnerPaypalCsv -NoTypeInformation
 }
 
-$data | Select-Object "Recipient Email", "Recipient First Name", "Recipient Last Name", "Invoice Number", "Due Date", "Reference",
- "Item Name", "Description", "Item Amount", "Shipping Amount", "Discount","Currency","Note to Customer","Terms and Conditions", "Memo to Self" | Export-CSV -Path $WinnerPaypalCsv -NoTypeInformation
-}
-
+#One second delay
+Start-Sleep -s 1
 
 Write-Host "
- ____ ____ ____                     
-||T |||h |||e ||                    
-||__|||__|||__||                    
-|/__\|/__\|/__\|                    
- ____ ____ ____ ____ ____ ____ ____ 
+ ____ ____ ____
+||T |||h |||e ||
+||__|||__|||__||
+|/__\|/__\|/__\|
+ ____ ____ ____ ____ ____ ____ ____
 ||W |||i |||n |||n |||e |||r |||s ||
 ||__|||__|||__|||__|||__|||__|||__||
 |/__\|/__\|/__\|/__\|/__\|/__\|/__\|
- ____ ____ ____ ____                
-||H |||a |||v |||e ||               
-||__|||__|||__|||__||               
-|/__\|/__\|/__\|/__\|               
- ____ ____ ____ ____                
-||B |||e |||e |||n ||               
-||__|||__|||__|||__||               
-|/__\|/__\|/__\|/__\|               
- ____ ____ ____ ____ ____ ____      
-||C |||h |||o |||s |||e |||n ||     
-||__|||__|||__|||__|||__|||__||     
-|/__\|/__\|/__\|/__\|/__\|/__\|     
+ ____ ____ ____ ____
+||H |||a |||v |||e ||
+||__|||__|||__|||__||
+|/__\|/__\|/__\|/__\|
+ ____ ____ ____ ____
+||B |||e |||e |||n ||
+||__|||__|||__|||__||
+|/__\|/__\|/__\|/__\|
+ ____ ____ ____ ____ ____ ____
+||C |||h |||o |||s |||e |||n ||
+||__|||__|||__|||__|||__|||__||
+|/__\|/__\|/__\|/__\|/__\|/__\|
 "
-start-sleep -s 1
-#Opens Csv file
-Invoke-Item $WinnerPaypalCsv
+
+#Wait 1 second
+Start-Sleep -s 1
